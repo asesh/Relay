eb58b51ff4f1cb0ca28cd511b57450c6d240e2f2
Asesh Shrestha <aseshshres@gmail.com>
2026-05-28 15:35:12 +0545

    Initial Commit

---
+++ Relay/RelayApp.swift
@@ -0,0 +1,32 @@
+//
+//  RelayApp.swift
+//  Relay
+//
+//  Created by Asesh Shrestha on 28/05/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RelayApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ RelayTests/RelayTests.swift
@@ -0,0 +1,16 @@
+//
+//  RelayTests.swift
+//  RelayTests
+//
+//  Created by Asesh Shrestha on 28/05/2026.
+//
+
+import Testing
+
+struct RelayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
